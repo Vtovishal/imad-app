4213ff094e58fe3c29a4e6f293dbdee3d7c84bbb
Vtovishal <33997670+Vtovishal@users.noreply.github.com>
2018-02-16 12:57:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,10 +8,10 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function(req,req){
+app.get('/article-one',function(req,res){
     res.send("Article one is served");
 });
-app.get('/article-two',function(req,req){
+app.get('/article-two',function(req,res){
     res.send("Article two is served");
 });
 app.get('/article-three',function(req,req){
