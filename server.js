345310dd9a347d19e7baf3f70c63e682273a27ce
Vtovishal <33997670+Vtovishal@users.noreply.github.com>
2018-02-16 13:14:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one',function(req,res){
 app.get('/article-two',function(req,res){
     res.send("Article two is served");
 });
-app.get('/article-three',function(req,req){
+app.get('/article-three',function(req,res){
     res.send("Article three is served");
 });
 
