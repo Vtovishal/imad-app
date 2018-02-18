41d510ba4d3f2e41e96da74ee7224c53ac59b584
Vtovishal <33997670+Vtovishal@users.noreply.github.com>
2018-02-18 14:49:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,12 @@ element.innerHTML = 'New Value';
 
 //image move
 var img = document.getElementById("move");
+function moveRight(){
+    marginLeft=marginLeft+  10;
+    img.style.marginLeft=marginLeft + 'px'
+}
+
 img.onclick = function () {
+  //  var interval = setInterval(moveRight,100)
     img.style.marginLeft= '100px' ;
 };
